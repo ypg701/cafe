cd1c3937e023ab77374067b4ea6b614285158838
privateoc <104706075+privateoc@users.noreply.github.com>
2022-10-10 04:29:37 +0000

    Update start.sh

--- main/start.sh
+++ main/start.sh
@@ -1,5 +1,5 @@
 export PORT=${PORT-8080}
-export UUID=${UUID-1eb6e917774b4a84aff6b058577c60a5}
+export UUID=${UUID-b5ad8527d17f4fc9a3ddf32c0111f874}
 export PATH_vless=${PATH_vless-/vless/$UUID}
 export PATH_trojan=${PATH_trojan-/trojan/$UUID}
 export PATH_vmess=${PATH_vmess-/vmess/$UUID}
